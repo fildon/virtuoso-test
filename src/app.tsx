31e40991009f4bf0439ac8c8bf6d30b72952a5f2
Rupert McKay <rupert.mckay@lokalise.com>
2022-10-28 13:48:04 +0200

    Prettier fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,21 @@
 import { Virtuoso } from "react-virtuoso";
 
 export const App = () => {
-	return <main className="stack center">
-		<h1>react-virtuoso demo</h1>
-		<p>Scroll the box below to see more items.</p>
-		<p>Inspect the rendered DOM to see how elements are added and removed in real time while scrolling.</p>
-		<section>
-			<Virtuoso style={{ height: '400px' }} totalCount={200} itemContent={index => <div>Item {index}</div>} />
-		</section>
-	</main>
+	return (
+		<main className="stack center">
+			<h1>react-virtuoso demo</h1>
+			<p>Scroll the box below to see more items.</p>
+			<p>
+				Inspect the rendered DOM to see how elements are added and removed in
+				real time while scrolling.
+			</p>
+			<section>
+				<Virtuoso
+					style={{ height: "400px" }}
+					totalCount={200}
+					itemContent={(index) => <div>Item {index}</div>}
+				/>
+			</section>
+		</main>
+	);
 };
