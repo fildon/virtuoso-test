8585ced727357b76499d3019c71dc43a4693b22c
Rupert McKay <rupert.mckay@lokalise.com>
2022-10-28 13:45:20 +0200

    Reduce test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,20 @@
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
 	verbose: true,
-	preset: 'ts-jest',
-	setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
-	testEnvironment: 'jsdom',
+	preset: "ts-jest",
+	setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+	testEnvironment: "jsdom",
 	collectCoverage: true,
-	collectCoverageFrom: ['**/src/**'],
+	collectCoverageFrom: ["**/src/**"],
 	// This is the imperative shell and will not be tested
-	coveragePathIgnorePatterns: ['src/index.tsx'],
+	coveragePathIgnorePatterns: ["src/index.tsx"],
 	coverageThreshold: {
 		global: {
 			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
+			functions: 50,
+			lines: 75,
+			statements: 80,
 		},
 	},
 };
