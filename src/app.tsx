b42e40c887fcc996a8eb785b49c5e9da3aa1519c
Rupert McKay <rupert.mckay@lokalise.com>
2022-10-28 14:08:27 +0200

    Adds link to the source code in the main view

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ export const App = () => {
 	return (
 		<main className="stack center">
 			<h1>react-virtuoso demo</h1>
+			<p>This is a minimal proof of concept usage of react-virtuoso. You can inspect the source code over at <a href="https://github.com/fildon/virtuoso-test">virtuoso-test</a></p>
 			<p>Scroll the box below to see more items.</p>
 			<p>
 				Inspect the rendered DOM to see how elements are added and removed in
